730e059b15c5a6f95b36823132f951314253bc4e
davidfiliphe <davidfeliphe51@gmail.com>
2024-03-15 18:14:07 -0300

    Atualizando..

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # TP_FINAL_BACKEND
 
-TRABALHO FEITO POR: David Filiphe 
+GRUPO: Gleison Brito
--- criar_tabelas.php
+++ criar_tabelas.php
@@ -1,7 +1,7 @@
-<?php 
-   $pdo = new PDO("sqlite:trabalho.db");
+<?php
+    $pdo = new PDO("sqlite:estoque.db");
 
-   $queryCriaTabelaCategorias = "CREATE TABLE IF NOT EXISTS categorias (
+    $queryCriaTabelaCategorias = "CREATE TABLE IF NOT EXISTS categorias (
         id	INTEGER NOT NULL,
         nome	TEXT NOT NULL,
         PRIMARY KEY(id AUTOINCREMENT)
@@ -9,26 +9,25 @@
 
     $pdo->query($queryCriaTabelaCategorias);
 
-     $queryCriaTabelaProdutos = "CREATE TABLE IF NOT EXISTS produtos (
+    $queryCriaTabelaProdutos = "CREATE TABLE IF NOT EXISTS produtos (
         id	INTEGER NOT NULL,
         nome	TEXT NOT NULL,
         descricao	TEXT,
         preco	REAL NOT NULL,
         categoria_id	INTEGER,
-        FOREIGN KEY(categoria_id) REFERENCES categorias(id),
-        PRIMARY KEY(id AUTOINCREMENT)
+        PRIMARY KEY(id AUTOINCREMENT),
+        FOREIGN KEY(categoria_id) REFERENCES categorias(id)
     )";
 
     $pdo->query($queryCriaTabelaProdutos);
-
+    
     $queryCriaTabelaEstoque = "CREATE TABLE IF NOT EXISTS estoque (
         id	INTEGER NOT NULL,
         quantidade	INTEGER NOT NULL,
         produto_id	INTEGER,
-        PRIMARY KEY(id AUTOINCREMENT),
-        FOREIGN KEY(produto_id) REFERENCES produtos(id)
+        FOREIGN KEY(produto_id) REFERENCES produtos(id),
+        PRIMARY KEY(id AUTOINCREMENT)
     )";
 
     $pdo->query($queryCriaTabelaEstoque);
- 
 ?>
